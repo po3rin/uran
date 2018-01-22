0e0094d9ef8d8e1f5581f6c53f004ad8e315373a
中村弘武 <abctail30@gmail.com>
2018-01-22 23:45:08 +0900

    add mp4 control

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,7 @@ module.exports = {
     ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'manifest', href: '/manifest.json' },
       { href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel: 'stylesheet' },
       { href: 'https://fonts.googleapis.com/css?family=Roboto:300,400', rel: 'stylesheet' }
